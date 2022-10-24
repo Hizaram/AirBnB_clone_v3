5addf996fcfe2abdad7811659b7c6bf1114d43e9
Hizaram <promisevector@gmail.com>
2022-10-24 21:55:53 +0100

    Fixed the app.teardown_appcontext error

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-@app.teardown_context
+@app.teardown_appcontext
 def teardown(error):
     """Function that stops the current session
     Arguments:
